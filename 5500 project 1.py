066f338adbc4fd85023950e4faff4f6b54f7166c
xuel12 <xue_xl@hotmail.com>
2020-05-17 21:18:38 -0400

    extract intensity from mzML

--- 5500 project 1.py
+++ 5500 project 1.py
@@ -5,20 +5,29 @@
 
 
 import pyteomics
+import sys
 import os
 import numpy as np
 import pandas as pd
 from pyteomics import tandem
 import pymzml
 import argparse
-os.chdir('F://')
+
+try: 
+    # os.chdir('F://')
+    os.chdir('/Users/xuel12/Documents/MSdatascience/DS5500datavis/project1')
+    print("Current directory is {}".format(os.getcwd()))
+except: 
+    print("Something wrong with specified directory. Exception- ", sys.exc_info())
+    
+
 
 
 # In[154]:
 
 
 from pyteomics import mzml, auxiliary
-with mzml.read('01625b_GA1-TUM_first_pool_1_01_01-DDA-1h-R2.mzML') as reader:
+with mzml.read('data/01625b_GB2-TUM_first_pool_10_01_01-DDA-1h-R2.mzML') as reader:
     auxiliary.print_tree(next(reader))
 #     reader['scanList']
 
@@ -26,8 +35,20 @@ with mzml.read('01625b_GA1-TUM_first_pool_1_01_01-DDA-1h-R2.mzML') as reader:
 # In[153]:
 
 
-reader = mzml.read('01625b_GA1-TUM_first_pool_1_01_01-DDA-1h-R2.mzML') 
-reader.binary_array_record.decode
+reader = mzml.read('data/01625b_GB2-TUM_first_pool_10_01_01-DDA-1h-R2.mzML')
+mzlist = {}
+intensitylist = {}
+i = 0
+for spec in reader:
+    if (spec['ms level'] == 2):
+        mzlist[i] = spec['m/z array'].tolist()
+        intensitylist[i] = spec['intensity array'].tolist()
+    i += 1
+
+   
+
+# reader.binary_array_record.decode
+
 # print(next(reader))
 # # dir(reader)
 # # print(reader.schema_info)
